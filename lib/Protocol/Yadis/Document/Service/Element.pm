7bbdb4f8338dca2ec7d8708b903e6f3a418c86c8
vti <viacheslav.t@gmail.com>
2009-08-07 17:18:26 +0200

    name change

--- lib/Protocol/Yadis/Document/Service/Element.pm
+++ lib/Protocol/Yadis/Document/Service/Element.pm
@@ -107,11 +107,11 @@ String representation.
 
 =head1 AUTHOR
 
-Viacheslav Tikhanovskii, C<vti@cpan.org>.
+Viacheslav Tykhanovskyi, C<vti@cpan.org>.
 
 =head1 COPYRIGHT
 
-Copyright (C) 2009, Viacheslav Tikhanovskii.
+Copyright (C) 2009, Viacheslav Tykhanovskyi.
 
 This program is free software, you can redistribute it and/or modify it under
 the same terms as Perl 5.10.
