9591dd6b43c4683810292c4f67e338efd7be58d7
vti <viacheslav.t@gmail.com>
2010-02-21 12:06:30 +0100

    Updated version

--- lib/Protocol/Yadis.pm
+++ lib/Protocol/Yadis.pm
@@ -9,7 +9,7 @@ use constant DEBUG => $ENV{PROTOCOL_YADIS_DEBUG} || 0;
 
 use Protocol::Yadis::Document;
 
-our $VERSION = '0.990101';
+our $VERSION = '0.990102';
 
 sub new {
     my $class = shift;
